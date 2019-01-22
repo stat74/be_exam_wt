b34ae1a8fa391aad2ca4f4fc86fcba1f48d9583c
stat74 <46922945+stat74@users.noreply.github.com>
2019-01-22 14:16:03 -0500

    Create CSVConstants.java

---
+++ score/CSVConstants.java
@@ -0,0 +1,28 @@
+
+package score;
+
+public class CSVConstants {
+
+	public static final String FIRST = "first";
+	public static final String MIDDLE = "middle";
+	public static final String LAST = "last";
+	public static final String ID = "id";
+	public static final String PHONE = "phone";
+	
+	public static final String CSV = ".csv";
+	public static final String JSON = ".json";
+	
+	public static final String MIDDLENAME = "middleName";
+	
+	public static final String ID_ERROR = "id must be a positive numerical value with exactly eight digits";
+	public static final String PHONE_ERROR = "phone must be in the format ###-###-####";
+	
+	public static final String ERROR_DIRECTORY = "error-directory/";
+	public static final String OUTPUT_DIRECTORY = "output-directory/";
+	public static final String INPUT_DIRECTORY = "input-directory/";
+
+	public static final String LINE_NUM = "LINE_NUM";
+	public static final String ERROR_MSG = "ERROR_MSG";
+	public static final String DELIMITER = ",";
+	public static final String END_LINE = "\n";
+}
