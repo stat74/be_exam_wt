8e8654bd131718fe38d337bf38fc5b0875c9ee51
stat74 <46922945+stat74@users.noreply.github.com>
2019-01-23 10:16:30 -0500

    Update CSVErrorWriter.java

--- score/CSVErrorWriter.java
+++ score/CSVErrorWriter.java
@@ -2,6 +2,7 @@
 package score;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
@@ -13,8 +14,13 @@ public class CSVErrorWriter {
 
 	public void writeErrorLog(String fileName, List<CSVParsingError> parsingProblems) {
 
+		File directory = new File(CSVConstants.ERROR_DIRECTORY);
+		if (!directory.exists()) {
+			directory.mkdir();
+		}
+		
 		try (Writer writer = new BufferedWriter(
-				new OutputStreamWriter(new FileOutputStream(".\\error-directory\\"+fileName+CSVConstants.CSV), "utf-8"))) {
+				new OutputStreamWriter(new FileOutputStream(CSVConstants.ERROR_DIRECTORY+fileName+CSVConstants.CSV), CSVConstants.FILE_FORMAT))) {
 			writer.write(
 					CSVConstants.LINE_NUM + CSVConstants.DELIMITER + CSVConstants.ERROR_MSG + CSVConstants.END_LINE);
 			for (CSVParsingError oneError : parsingProblems) {
